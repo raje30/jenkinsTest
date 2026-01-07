a8d3ac024665a530a6d2d3f32528a188f6f209b8
raje30 <abhi.efficacy@gmail.com>
2026-01-07 22:30:16 +0530

    initial commit 3

--- main.java
+++ main.java
@@ -3,6 +3,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello Abhinav");
     System.out.println("Welcome to jenkins with git");
+    System.out.println("Enjoy Devops buddy");
   }
   
 }
